d7b6967446be03b8520e5d6496a9ecb0be2b911b
Nikita Semakov <n.semakov@yahoo.com>
2015-08-02 17:09:03 +0300

    Initial Commit

---
+++ 23. Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  23. Gestures
+//
+//  Created by Admin on 02.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 23. Gestures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  23. Gestures
+//
+//  Created by Admin on 02.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
